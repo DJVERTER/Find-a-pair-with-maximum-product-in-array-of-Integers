5f86f389e84ef1f59eec544aeb1294335d91bbce
DJVERTER <creativekira1982@gmail.com>
2021-12-13 10:27:40 +0200

    without method

--- AmdarisTest/Program.cs
+++ AmdarisTest/Program.cs
@@ -6,13 +6,12 @@ namespace ArrayAT
 {
     class Program
     {
-        static void MaxProduct(int []arr, int n)
+        public static void Main()
         {
-            Console.WriteLine(n);
-            int a = arr[0], b = arr[1];
-            Console.WriteLine(arr[0]);
-            Console.WriteLine(arr[1]);
-
+            int[] arr = { 1, -5, 8, -20, 13 };
+            int n = arr.Length;
+            int a = arr[0];
+            int b = arr[1];
             for (int i = 0; i < n; i++)
                 for (int j = i + 1; j < n; j++)
                     if (arr[i] * arr[j] > a * b)
@@ -20,14 +19,7 @@ namespace ArrayAT
                         a = arr[i];
                         b = arr[j];
                     }
-            Console.Write("Max product pair is {" +
-                       a + ", " + b + "}");
-        }
-        public static void Main()
-        {
-            int[] arr = { 1, -5, 8, -20, 13 };
-            int n = arr.Length;
-            MaxProduct(arr, n);
+            Console.Write("Max product pair is {" + a + ", " + b + "}");
         }
     }
 }
